c378fe4a918fbdc382cce3f268adee8799e8814b
mostafizul <siamahmed365@gmail.com>
2020-05-12 22:34:08 +0600

    use of console

---
+++ practice.js
@@ -0,0 +1,12 @@
+/*
+//use of console
+console.log("siam");
+console.log(123);
+console.log(true);
+var siam = "siam is good";
+console.log(siam);
+console.error("this is error");
+console.warn("this is warning");
+console.clear();
+console.table({a:1,b:2});
+*/
