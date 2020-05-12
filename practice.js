396f3cb0f43b2ded617641c06e0976bca279171f
mostafizul <siamahmed365@gmail.com>
2020-05-13 00:44:05 +0600

    JS Practice

--- practice.js
+++ practice.js
@@ -10,3 +10,16 @@ console.warn("this is warning");
 console.clear();
 console.table({a:1,b:2});
 */
+
+/*
+variables -> var, let, const
+var can't start with number,doesn't allow space or doesn't allow JS key word
+var siam = "he is a good boy";
+console.log(siam);
+let siam1 = "he is a good boy";
+console.log(siam);
+//have to assign a value
+const siam3 = "siam";
+console.log(siam3);
+*/
+
